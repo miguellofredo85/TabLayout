467e306e0b7814955f0127b4361a673417254fd4
Miguel Lofredo <miguellofredo@gmail.com>
2023-07-12 01:52:31 -0300

    Add fragments and colors

--- app/src/main/java/com/example/tablayout/MainActivity.java
+++ app/src/main/java/com/example/tablayout/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.tablayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+    TabItem tabChat = findViewById(R.id.chat), tabStatus = findViewById(R.id.status), tabCalls = findViewById(R.id.calls);
+    ViewPager viewPager = findViewById(R.id.viewPager);
     }
 }
---
+++ app/src/main/java/com/example/tablayout/MyPagerAdapter.java
@@ -0,0 +1,24 @@
+package com.example.tablayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+
+    public MyPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
